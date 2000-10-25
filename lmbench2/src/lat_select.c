8d21a6613de8f87c1366dada83a5e5f0ea7c1fe6
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2000-10-25 10:33:34 +0000

    - Enhance lat_select so it can select either on a file or on a TCP   socket. - Modify getsummary so the TCP socket select latency is used rather   than the file latency (select() is more commonly used for networking   than for files)
    (Logical change 1.31)
    
    
    git-svn-id: https://svn.code.sf.net/p/lmbench/code/trunk@567 9cc621ae-7264-4df2-8f7e-c588375cf588

--- lmbench2/src/lat_select.c
+++ lmbench2/src/lat_select.c
@@ -1,7 +1,7 @@
 /*
  * lat_select.c - time select system call
  *
- * usage: lat_select [n]
+ * usage: lat_select tcp|file [n]
  *
  * Copyright (c) 1996 Larry McVoy.  Distributed under the FSF GPL with
  * additional restriction that results may published only if
@@ -23,24 +23,93 @@ doit(int n, fd_set *set)
 int
 main(int ac, char **av)
 {
-	int	i, last = 0 /* lint */;
-	int	N = 200, fd;
+	int	n, N, last, fd, fid;
+	pid_t	pid, ppid;
 	fd_set	set;
-	char	buf[256];
+	char	buf[L_tmpnam+256];
+	char	fname[L_tmpnam];
+	char*	report_file = "Select on %d fd's";
+	char*	report_tcp  = "Select on %d tcp fd's";
+	char*	report;
+	char*	usage = "lat_select tcp|file [n]\n";
 
 	morefds();
-	if (ac > 1) N = atoi(av[1]);
+	N = 200;
+	fname[0] = 0;
+	pid = 0;
+	report = report_file;
+
+	if (ac != 2 && ac != 3) {
+		fprintf(stderr, usage);
+		exit(1);
+	}
+
+	if (streq(av[1], "tcp")) {
+		report = report_tcp;
+		
+		/* Create a socket for clients to connect to */
+		fd = tcp_server(TCP_CONNECT, SOCKOPT_NONE);
+		if (fd <= 0) {
+			perror("lat_select: Could not open tcp server socket");
+			exit(1);
+		}
+
+		/* Start server process to accept client connections */
+		ppid = getpid();
+		switch(pid = fork()) {
+		case 0:
+			/* child server process */
+			while (ppid == getppid()) {
+				int newsock = tcp_accept(fd, SOCKOPT_NONE);
+				read(newsock, &fid, 1);
+				close(newsock);
+			}
+			close(fd);
+			exit(0);
+		case -1:
+			/* error */
+			perror("lat_select::server(): fork() failed");
+			exit(1);
+		default:
+			break;
+		}
+		close(fd);
+		fd = tcp_connect("localhost", TCP_CONNECT, SOCKOPT_NONE);
+		if (fd <= 0) {
+			perror("lat_select: Could not open socket");
+			exit(1);
+		}
+	} else if (streq(av[1], "file")) {
+		/* Create a temporary file for clients to open */
+		tmpnam(fname);
+		fd = open(fname, O_RDWR|O_APPEND|O_CREAT, 0666);
+		if (fd <= 0) {
+			char buf[L_tmpnam+128];
+			sprintf(buf, 
+				"lat_select: Could not create temp file %s", fname);
+			perror(buf);
+			exit(1);
+		}
+	} else {
+		fprintf(stderr, usage);
+		exit(1);
+	}
+
+	if (ac == 3) N = atoi(av[2]);
+
 	FD_ZERO(&set);
-	for (i = 3; i < 50; ++i) close(i);
-	for (fd = 0; fd < N; fd++) {
-		i = dup(0);
-		if (i == -1) break;
-		last = i;
-		FD_SET(i, &set);
+	last = 0;
+	for (n = 0; n < N; n++) {
+		fid = dup(fd);
+		if (fid == -1) break;
+		if (fid > last) last = fid;
+		FD_SET(fid, &set);
 	}
 	last++;
 	BENCH(doit(last, &set), 0);
-	sprintf(buf, "Select on %d fd's", fd);
+	sprintf(buf, report, n);
 	micro(buf, get_n());
+	if (pid) kill(SIGKILL, pid);
+	if (fname[0]) unlink(fname);
 	exit(0);
 }
