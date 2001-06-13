cad8ea19e77d9c3c20e4a579d16ec7210b3121ea
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2001-06-13 07:42:21 +0000

    Add new/improved handling of various available interfaces for random number generation lrand48()/rand()/random().
    (Logical change 1.68)
    
    
    git-svn-id: https://svn.code.sf.net/p/lmbench/code/trunk@758 9cc621ae-7264-4df2-8f7e-c588375cf588

--- lmbench2/src/lmdd.c
+++ lmbench2/src/lmdd.c
@@ -64,8 +64,6 @@ void		flush(void);
 #define	VALLOC	malloc
 #endif
 
-extern	double	drand48();
-
 #ifdef	__sgi
 #	define	LSEEK(a,b,c)	(uint64)lseek64(a, (off64_t)b, c)
 #	define	ATOL(s)		atoll(s)
@@ -896,3 +894,4 @@ error(char *s)
 	perror(s);
 	exit(1);
 }
+
