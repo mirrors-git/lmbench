5c289a9ac61d3f56dd5aeb311d78b4fa202161f6
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2002-06-12 10:15:11 +0000

    Fix typo in BENCH1() macro
    (Logical change 1.82)
    
    
    git-svn-id: https://svn.code.sf.net/p/lmbench/code/trunk@881 9cc621ae-7264-4df2-8f7e-c588375cf588

--- lmbench2/src/bench.h
+++ lmbench2/src/bench.h
@@ -195,7 +195,7 @@ void	get_results(result_t *r);
 	BENCH_INNER(loop_body, enough);  				\
 	__usecs = gettime();						\
 	__usecs -= t_overhead() + get_n() * l_overhead();		\
-	settime(__usecs >= 0. ? (uint64)__usecs : 0.);			\
+	settime(__usecs >= 0. ? (uint64)__usecs : 0);			\
 }
 	
 #define	BENCH_INNER(loop_body, enough) { 				\
