e73fee74fe70205590103f33a7f0a311b26881ee
hp.com!staelin <hp.com!staelin@9cc621ae-7264-4df2-8f7e-c588375cf588>
2002-10-16 14:20:44 +0000

    bw_tcp.c: - tweak calculation of 'move' so it is a multiple of xfersize - change 'enough' from LONGER to 0 since 'move' is sized to   take roughly LONGER micro-seconds - add SOCKOPT_READ and SOCKOPT_WRITE to tcp_connect and tcp_server   as appropriate
    (Logical change 1.88)
    
    
    git-svn-id: https://svn.code.sf.net/p/lmbench/code/trunk@942 9cc621ae-7264-4df2-8f7e-c588375cf588

--- lmbench2/src/bw_tcp.c
+++ lmbench2/src/bw_tcp.c
@@ -30,11 +30,13 @@ transfer(uint64 move, char *server)
 	uint64	todo;
 
 	todo = move;
-	/*printf("Move %lu MB\n", (unsigned long)(move>>20));
-	*/
-	data = tcp_connect(server, TCP_DATA, SOCKOPT_READ|SOCKOPT_REUSE);
+	data = tcp_connect(server, TCP_DATA, SOCKOPT_READ|SOCKOPT_WRITE|SOCKOPT_REUSE);
+	if (data < 0) {
+		perror("bw_tcp: transfer: could not open socket");
+		return;
+	}
 	(void)sprintf(buf, "%llu", move);
-	if (write(data, buf, strlen(buf)) != strlen(buf)) {
+	if (write(data, buf, strlen(buf) + 1) != strlen(buf) + 1) {
 		perror("control write");
 		exit(1);
 	}
@@ -73,7 +75,7 @@ client_main(int ac, char **av)
 
 		move = 0;
 		server = &av[1][1];
-		data = tcp_connect(server, TCP_DATA, SOCKOPT_NONE);
+		data = tcp_connect(server, TCP_DATA, SOCKOPT_REUSE);
 		write(data, "0", 1);
 		exit(0);
 	} else {
@@ -86,19 +88,24 @@ client_main(int ac, char **av)
 	start(0);
 	transfer(move, server);
 	usecs = stop(0,0);
-	fprintf(stderr, "initial bandwidth measurement: move=%llu, usecs=%llu: ", move, usecs);
 	save_n(1);
 	settime(usecs);
+	/*
+	fprintf(stderr, "initial bandwidth measurement: move=%llu, usecs=%llu: ", move, usecs);
 	mb(move);
+	/* XXX */
 	if (usecs >= LONGER) {	/* must be 10Mbit ether or sloooow box */
 		save_n(1);
 		goto out;
 	}
-	usecs = LONGER / usecs;
+	usecs = LONGER / usecs + 1;
 	move *= usecs * 1.05;
 	if (move < MINMOVE) move = MINMOVE;
-	fprintf(stderr, "move=%llu, XFERSIZE=%d\n", move, XFERSIZE);
-	BENCH(transfer(move, server), LONGER);
+	if (move % XFERSIZE) move += XFERSIZE - (move % XFERSIZE);
+	BENCH(transfer(move, server), 0);
+	/*
+	fprintf(stderr, "move=%llu, n=%llu, usecs=%llu, XFERSIZE=%d\n", move, get_n(), gettime(), XFERSIZE);
+	/* XXX */
 out:	(void)fprintf(stderr, "Socket bandwidth using %s: ", server);
 	mb(move * get_n());
 	exit(0);
@@ -121,10 +128,10 @@ server_main(int ac, char **av)
 	GO_AWAY;
 
 	signal(SIGCHLD, child);
-	data = tcp_server(TCP_DATA, SOCKOPT_WRITE|SOCKOPT_REUSE);
+	data = tcp_server(TCP_DATA, SOCKOPT_READ|SOCKOPT_WRITE|SOCKOPT_REUSE);
 
 	for ( ;; ) {
-		newdata = tcp_accept(data, SOCKOPT_WRITE);
+		newdata = tcp_accept(data, SOCKOPT_WRITE|SOCKOPT_READ);
 		switch (fork()) {
 		    case -1:
 			perror("fork");
@@ -168,7 +175,9 @@ source(int data)
 		kill(getppid(), SIGTERM);
 		exit(0);
 	}
-	printf("server: nbytes=%llu\n", nbytes);
+	/*
+	fprintf(stderr, "server: nbytes=%llu, buf=%s\n", nbytes, buf); fflush(stderr);
+	/* XXX */
 	for (; (n = write(data, buf, XFERSIZE)) > 0 && nbytes > n; nbytes-=n) {
 #ifdef	TOUCH
 		touch(buf, XFERSIZE);
